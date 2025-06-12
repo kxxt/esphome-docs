37085fe290492ad425bff59adc7a8871af8e29f7
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-06-12 15:34:14 +1000

    Remove fossils

--- tools/migrate.sh
+++ tools/migrate.sh
@@ -12,6 +12,7 @@ tools/convert_rst_to_md.py . . --replace
 # Must commit the move before the rewrites
 git commit -m "Rename files"
 # Now add the updated content and commit
+rm -rf _* components guides cookbook changelog automations images index.rst markdown.py projects svg2png svg2png.py web-api
 git add -u
 git commit -m "Add changed files"
 
