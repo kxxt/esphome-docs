efc20a0052737cd7769c3bba6a35e5e596124d08
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-06-13 15:37:14 +1000

    better version script

--- tools/migrate.sh
+++ tools/migrate.sh
@@ -13,23 +13,23 @@ tools/convert_rst_to_md.py . . --replace
 git commit --message="Rename files" --author="esphomebot <68923041+esphomebot@users.noreply.github.com>"
 
 # Extract version and release values from conf.py
-eval "$(python3 -c '
-import sys
-import re
-with open("conf.py") as f:
-    for l in f:
-        m = re.match(r"version\s*=\s*\"([^\"]+)\"", l)
-        if m:
-            print("version=\"{}\"".format(m.group(1)))
-        m = re.match(r"release\s*=\s*\"([^\"]+)\"", l)
-        if m:
-            print("release=\"{}\"".format(m.group(1)))
-' 2>/dev/null)"
-
 # Write to data/version.yaml
 mkdir -p data
-echo "version: $version
-release: $release" > data/version.yaml
+python3 -c '
+import sys
+import yaml
+import conf
+try:
+  with open("data/version.yaml", "r") as file:
+      data = yaml.safe_load(file)
+except FileNotFoundError:
+    data = {}
+data["release"] = conf.release
+data["version"] = conf.version
+with open("data/version.yaml", "w") as file:
+    yaml.dump(data, file)
+'
+
 # Now add the updated content and commit
 rm -rf _* components guides cookbook changelog automations images index.rst markdown.py projects svg2png svg2png.py web-api
 git add -u
