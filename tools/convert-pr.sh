1d0f9267146178f7eda3f659a04939a1c5364e77
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-08-14 11:49:20 +1000

    Add instructions

--- tools/convert-pr.sh
+++ tools/convert-pr.sh
@@ -4,6 +4,13 @@
 # Start by checking out the PR branch, either from your own repo, or by using the
 # gh command, e.g `gh pr checkout https://github.com/esphome/esphome-docs/pull/<pr number>
 # Then run this script.
+#
+# IMPORTANT:
+# run `git diff next` afterwards to check just what changes you have made.
+# If your PR modifies existing files, it's quite possible you will have
+# overwritten any changes made since your PR was created, and the conversion
+# process just overwrites the .md file, rather than doing a normal merge.
+# So check carefully and manually fix any issues, or just revise the PR from scratch.
 
 
 git config merge.directoryRenames false
